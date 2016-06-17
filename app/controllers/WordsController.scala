ded7a9bde55c34a70c5907d5c0677f23bcb9b932
Markus Klinga <laite@gmx.com>
2016-06-17 14:09:14 +0300

    Get random words from the database

--- app/controllers/WordsController.scala
+++ app/controllers/WordsController.scala
@@ -9,15 +9,40 @@ import scala.util.Success
 import play.api.libs.json._
 import dao.Words
 import dao.Conjugations
+import models._
 
 @Singleton
 class WordsController @Inject() (wordsDao: Words, conjugationsDao: Conjugations)(implicit exec: ExecutionContext) extends Controller {
 
+  private def appendConjugationsToWord (word: Word, conjugations: Seq[Conjugation]): JsObject = {
+    JsObject(Seq("word" -> Json.toJson(word), "conjugations" -> Json.toJson(conjugations)))
+  }
+
+  private def appendConjugationsToWords (words: Seq[Word], conjugations: Seq[Seq[Conjugation]]): JsArray = {
+    val combined: Seq[Tuple2[Word, Seq[Conjugation]]] = for {
+      w <- words
+      wordConjugations: Seq[Conjugation] = conjugations
+        .fold(Seq[Conjugation]())((old: Seq[Conjugation], value: Seq[Conjugation]) => {
+          if (value(0).word_id == w.id.get) value
+          else old
+        })
+    } yield (w, wordConjugations)
+
+    Json.arr(combined.map(cs => appendConjugationsToWord(cs._1, cs._2)))
+  }
+
   def index = Action.async {
-    val words = wordsDao.all()
+    wordsDao.all().map(words => Ok(Json.toJson(words)))
+  }
+
+  def getRandom (languageId: Int, amount: Int) = Action.async {
+    val words: Future[Seq[Word]] = wordsDao.getRandomByLanguage(languageId, amount)
     words
-      .flatMap(ws => Future.sequence(ws.map(word => conjugationsDao.findByWord(word.id.get))))
-      .map(result => Ok(Json.toJson(result)))
+      .flatMap((ws: Seq[Word]) =>
+        Future.sequence(ws.map(
+          (word: Word) => conjugationsDao.findByWord(word.id.get))))
+      .flatMap((conjugations: Seq[Seq[Conjugation]]) =>
+          words.map((ws: Seq[Word]) => Ok(appendConjugationsToWords(ws, conjugations))))
   }
 
   def getOne (id: Int) = Action.async {
@@ -26,15 +51,7 @@ class WordsController @Inject() (wordsDao: Words, conjugationsDao: Conjugations)
     word.flatMap(w => {
       conjugationsDao
         .findByWord(w.id.get)
-        .map(cs => Ok(
-          JsObject(
-            Seq(
-              "word" -> Json.toJson(w),
-              "conjugations" -> Json.toJson(cs)
-            )
-          )
-        )
-      )
+        .map(cs => Ok(appendConjugationsToWord(w, cs)))
     })
   }
 }
--- app/dao/WordsDAO.scala
+++ app/dao/WordsDAO.scala
@@ -19,9 +19,20 @@ class Words @Inject()(dbConfigProvider: DatabaseConfigProvider) extends Controll
 
   def all(): Future[Seq[Word]] = dbConfig.db.run(Words.result)
 
-  def find(id: Int): Future[Word] = {
-    val word = for ( w <- Words if (w.id === id) ) yield w
+  def find (id: Int): Future[Word] = {
+    val word = for (w <- Words if (w.id === id)) yield w
     dbConfig.db.run(word.result.head)
+  }
+
+  def findByLanguage (languageId: Int): Future[Seq[Word]] = {
+    val word = for (w <- Words if w.language_id === languageId) yield w
+    dbConfig.db.run(word.result)
+  }
+
+  def getRandomByLanguage (languageId: Int, amount: Int): Future[Seq[Word]] = {
+    val rand = SimpleFunction.nullary[Double]("random")
+    val word = for (w <- Words if w.language_id === languageId) yield w
+    dbConfig.db.run(word.sortBy(x => rand).take(amount).result)
   }
 
   private class WordsTable (tag: Tag) extends Table[Word](tag, "words") {  
